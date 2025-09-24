03e5ff107be6ae655e4fd6f11ccd378d0f9b3115
mickaktus07 <llmmng0823@proton.me>
2025-09-24 19:18:56 +0200

    demo version

---
+++ src/fundingcomponents/Walletbtn.js
@@ -0,0 +1,10 @@
+export const Wallnetbtn = () => {
+
+  return (
+    <div className="another">
+      <div className="tabButton colortabBtn" >
+            Connect Wallet
+          </div>
+    </div>
+  );
+};
